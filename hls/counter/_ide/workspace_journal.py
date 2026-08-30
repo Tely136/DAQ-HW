d63ad5eebe8a9bca146fe84c3dc6d4a67b12d965
Thomas Ely <tely136@gmail.com>
2026-08-29 20:41:35 -0400

    added workspace file

--- hls/counter/_ide/workspace_journal.py
+++ hls/counter/_ide/workspace_journal.py
@@ -1,45 +1,41 @@
-# 2026-08-28T08:55:26.490853100
+# 2026-08-29T19:47:52.035097100
 import vitis
 
 client = vitis.create_client()
 client.set_workspace(path="counter")
 
-comp = client.create_hls_component(name = "counter",part = "xc7z007sclg400-1",cfg_file = ["hls_config.cfg"],template = "empty_hls_component")
+status = client.add_local_example_repo(name="hls_intro_examples_te", display_name="hls_intro_examples_te", description="Examples Repository.", type="HLS", local_directory="c:\Users\tely1\.Xilinx\Vitis\2026.1\Vitis-HLS-Introductory-Examples")
 
-comp = client.create_hls_component(name = "using_fixed_point",template = "vitis_hls_examples/Modeling/using_fixed_point")
+client.sync_git_example_repo(name="hls_intro_examples_te")
 
-cfg = client.get_config_file(path="C:\DAQ-HW\hls\counter\counter\hls_config.cfg")
+comp = client.create_hls_component(name = "using_fixed_point",template = "hls_intro_examples_te/Modeling/using_fixed_point")
 
-cfg.set_values(key="syn.file", values=["C:/Users/tely1/.Xilinx/Vitis/2026.1/vitis_hls_examples/Modeling/using_fixed_point/cpp_ap_fixed.cpp", "C:/Users/tely1/.Xilinx/Vitis/2026.1/vitis_hls_examples/Modeling/using_fixed_point/cpp_ap_fixed.h", "C:/Users/tely1/.Xilinx/Vitis/2026.1/vitis_hls_examples/Modeling/using_fixed_point/cpp_ap_fixed_test.cpp"])
+client.sync_git_example_repo(name="hls_intro_examples_te")
 
-comp = client.get_component(name="counter")
-comp.run(operation="C_SIMULATION")
+comp = client.create_hls_component(name = "using_fixed_point",template = "hls_intro_examples_te/Modeling/using_fixed_point")
 
-cfg = client.get_config_file(path="/c:/DAQ-HW/hls/counter/counter/hls_config.cfg")
+comp = client.create_hls_component(name = "using_fixed_point",template = "hls_intro_examples_te/Modeling/using_fixed_point")
+
+status = client.delete_example_repo(name="hls_intro_examples_te")
+
+comp = client.create_hls_component(name = "fixed_point_example",part = "xc7z007sclg400-1",cfg_file = ["hls_config.cfg"],template = "empty_hls_component")
+
+cfg = client.get_config_file(path="/c:/DAQ-HW/hls/counter/fixed_point_example/hls_config.cfg")
 
 cfg.set_value(section="hls", key="syn.top", value="cpp_ap_fixed")
 
-comp.run(operation="SYNTHESIS")
-
-comp = client.create_hls_component(name = "using_fixed_point",template = "vitis_hls_examples/Modeling/using_fixed_point")
-
-client.sync_git_example_repo(name="vitis_hls_examples")
-
-comp = client.create_hls_component(name = "using_fixed_point",template = "vitis_hls_examples/Modeling/using_fixed_point")
-
-cfg = client.get_config_file(path="C:\DAQ-HW\hls\counter\counter\hls_config.cfg")
-
-cfg.set_values(key="syn.file", values=["C:/Users/tely1/.Xilinx/Vitis/2026.1/vitis_hls_examples/Modeling/using_fixed_point/cpp_ap_fixed.cpp", "C:/Users/tely1/.Xilinx/Vitis/2026.1/vitis_hls_examples/Modeling/using_fixed_point/cpp_ap_fixed.h"])
-
-cfg.set_values(key="syn.blackbox.file", values=[])
+comp = client.get_component(name="fixed_point_example")
+comp.run(operation="C_SIMULATION")
 
 comp.run(operation="SYNTHESIS")
+
+comp.run(operation="CO_SIMULATION")
 
 comp.run(operation="PACKAGE")
 
 comp.run(operation="IMPLEMENTATION")
 
-client.delete_component(name="counter")
+client.delete_component(name="fixed_point_example")
 
-vitis.dispose()
+comp = client.create_hls_component(name = "counter_test",part = "xc7z007sclg400-1",cfg_file = ["hls_config.cfg"],template = "empty_hls_component")
 
